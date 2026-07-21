e5dbe96ecc4a23b9edfb18b45fc9d46493994fd9
Marvin Seifert <m.seifert@sussex.ac.uk>
2026-07-21 11:47:43 +0100

    Allows for precise timing stamps analysis

--- src/fpspy/load_timings.py
+++ src/fpspy/load_timings.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-d = np.load(r"/home/mawa/.local/state/fpspy/log/20260714T132647Z/frame_timings_win1.npz")
+import matplotlib.pyplot as plt
+# %%
+d = np.load(r"/home/mawa/.local/state/fpspy/log/20260720T145139Z/frame_timings_win1.npz")
 t, s = d["timings"], d["s_frames"]
 budget_left = s[1:] - t[
     :, 3]  # slack before each next deadline
@@ -9,3 +10,7 @@ render_time = t[:, 2] - t[
 refresh_period = np.nanmedian(np.diff(t[:, 3]))  # true display refresh, from vsync quantization
 
 
+# %%
+fig, ax = plt.subplots(figsize=(20, 10))
+ax.hist(np.diff(t[:, 3]), bins=np.arange(-1, 1, 0.05))
+fig.show()
